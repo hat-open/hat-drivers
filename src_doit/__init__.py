69e8c63d1d502e3ee31d60a90c9a8977e8a05065
bozo.kopic <bozo@kopic.xyz>
2021-11-02 14:59:03 +0100

    doit update

--- src_doit/__init__.py
+++ src_doit/__init__.py
@@ -42,8 +42,6 @@ def task_build():
         build_wheel(
             src_dir=src_py_dir,
             dst_dir=build_py_dir,
-            src_paths=list(common.path_rglob(src_py_dir,
-                                             blacklist={'__pycache__'})),
             name='hat-drivers',
             description='Hat communication drivers',
             url='https://github.com/hat-open/hat-drivers',
