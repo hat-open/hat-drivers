34e4ba6d1d978cf63a5cd44f410d38a1d0a72ce7
bozo.kopic <bozo@kopic.xyz>
2022-03-26 20:39:48 +0100

    requirements update

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ extensions = [
 
 version = (root_path / 'VERSION').read_text().strip()
 project = 'hat-drivers'
-copyright = '2020-2021, Hat Open AUTHORS'
+copyright = '2020-2022, Hat Open AUTHORS'
 master_doc = 'index'
 
 html_theme = 'furo'
