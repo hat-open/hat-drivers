ffaa7c3f5d365c79994fd0608df9891d355d6ee8
bozo.kopic <bozo@kopic.xyz>
2024-05-02 18:32:14 +0200

    modbus gateway error codes

--- src_py/hat/drivers/modbus/common.py
+++ src_py/hat/drivers/modbus/common.py
@@ -18,10 +18,12 @@ class DataType(enum.Enum):
 
 
 class Error(enum.Enum):
-    INVALID_FUNCTION_CODE = 1
-    INVALID_DATA_ADDRESS = 2
-    INVALID_DATA_VALUE = 3
-    FUNCTION_ERROR = 4
+    INVALID_FUNCTION_CODE = 0x01
+    INVALID_DATA_ADDRESS = 0x02
+    INVALID_DATA_VALUE = 0x03
+    FUNCTION_ERROR = 0x04
+    GATEWAY_PATH_UNAVAILABLE = 0x0a
+    GATEWAY_TARGET_DEVICE_FAILED_TO_RESPOND = 0x0b
 
 
 def apply_mask(value: int, and_mask: int, or_mask: int) -> int:
