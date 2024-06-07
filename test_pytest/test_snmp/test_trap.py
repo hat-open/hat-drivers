4497f446a5cc3ca82abea2396f1b48117d172e3b
Jakov Krstulovic Opara <jakov.krstulovic@koncar.hr>
2024-06-07 16:44:55 +0200

    snmp agent, trap tests update

--- test_pytest/test_snmp/test_trap.py
+++ test_pytest/test_snmp/test_trap.py
@@ -6,6 +6,7 @@ from hat import util
 from hat.drivers import udp
 from hat.drivers.snmp import common
 from hat.drivers.snmp import encoder
+from hat.drivers.snmp import key
 from hat.drivers.snmp import trap
 
 
@@ -121,11 +122,42 @@ async def test_sender_send_trap_v2c(udp_addr, data):
 
 
 @pytest.mark.parametrize("data", [list(_data('v3'))])
-async def test_sender_send_trap_v3(udp_addr, data):
+@pytest.mark.parametrize('auth, priv', [
+    (False, False),
+    (True, False),
+    (True, True)])
+@pytest.mark.parametrize('auth_type', common.AuthType)
+async def test_sender_send_trap_v3(udp_addr, data, auth, auth_type, priv):
+    username = 'user_xyz'
+    context = common.Context(engine_id=b'ctx_engine_id', name='name')
+    auth_pass = 'authpass'
+    priv_pass = 'privpass'
+    auth_key = (key.create_key(key_type=key.KeyType[auth_type.name],
+                               password=auth_pass,
+                               engine_id=context.engine_id)
+                if auth else None)
+    priv_key = (key.create_key(key_type=key.KeyType.DES,
+                               password=priv_pass,
+                               engine_id=context.engine_id)
+                if priv else None)
+
+    def on_auth_key(eid, usr):
+        return auth_key
+
+    def on_priv_key(eid, usr):
+        return priv_key
+
     listener = await udp.create(local_addr=udp_addr)
 
     sender = await trap.create_v3_trap_sender(
-        udp_addr, common.Context(engine_id=b'engine_id', name='name'))
+        remote_addr=udp_addr,
+        context=context,
+        user=common.User(
+            name=username,
+            auth_type=auth_type if auth else None,
+            auth_password=auth_pass if auth else None,
+            priv_type=common.PrivType.DES if priv else None,
+            priv_password=priv_pass if priv else None))
 
     sender.send_trap(common.Trap(cause=None,
                                  oid=(1, 2, 3, 4),
@@ -133,20 +165,21 @@ async def test_sender_send_trap_v3(udp_addr, data):
                                  data=data))
 
     msg_bytes, _ = await listener.receive()
-    msg = encoder.decode(msg_bytes)
+    msg = encoder.decode(msg_bytes,
+                         auth_key_cb=on_auth_key,
+                         priv_key_cb=on_priv_key)
 
     assert isinstance(msg, encoder.v3.Msg)
     assert msg.type == encoder.v3.MsgType.SNMPV2_TRAP
     assert msg.id is not None
     assert msg.reportable is False
-    assert msg.auth is False
-    assert msg.priv is False
-    assert msg.authorative_engine.id == b'engine_id'
+    assert msg.auth is auth
+    assert msg.priv is priv
+    assert msg.authorative_engine.id == context.engine_id
     assert msg.authorative_engine.boots == 0
     assert msg.authorative_engine.time is not None
-    assert msg.user == 'public'
-    assert msg.context.engine_id == b'engine_id'
-    assert msg.context.name == 'name'
+    assert msg.user == username
+    assert msg.context == context
     assert msg.pdu.error.type == common.ErrorType.NO_ERROR
 
     timestamp, oid, *pdu_data = msg.pdu.data
@@ -233,29 +266,52 @@ async def test_listener_receive_trap_v2c(udp_addr, data):
 
 
 @pytest.mark.parametrize("data", [list(_data('v3'))])
-async def test_listener_receive_trap_v3(udp_addr, data):
+@pytest.mark.parametrize('auth, priv', [(False, False),
+                                        (True, False),
+                                        (True, True)])
+@pytest.mark.parametrize('auth_type', common.AuthType)
+async def test_listener_receive_trap_v3(udp_addr, data, auth, auth_type, priv):
+    username = 'user_xyz'
+    engine_id = b'engine_id'
+    context = common.Context(engine_id=b'ctx_engine_id',
+                             name='name')
+    auth_pass = 'authpass'
+    priv_pass = 'privpass'
+    auth_key = (key.create_key(key_type=key.KeyType[auth_type.name],
+                               password=auth_pass,
+                               engine_id=engine_id)
+                if auth else None)
+    priv_key = (key.create_key(key_type=key.KeyType.DES,
+                               password=priv_pass,
+                               engine_id=engine_id)
+                if priv else None)
+    user = common.User(
+        name=username,
+        auth_type=auth_type if auth else None,
+        auth_password=auth_pass if auth else None,
+        priv_type=common.PrivType.DES if priv else None,
+        priv_password=priv_pass if priv else None)
+
     trap_queue = aio.Queue()
     listener = await trap.create_trap_listener(
         local_addr=udp_addr,
         v3_trap_cb=lambda _, user, context, tr: trap_queue.put_nowait(
             (user, context, tr)),
-        users=[common.User('user', None, None, None, None)])
+        users=[user])
 
     sender = await udp.create(remote_addr=udp_addr)
 
     msg = encoder.v3.Msg(type=encoder.v3.MsgType.SNMPV2_TRAP,
                          id=12345,
                          reportable=False,
-                         auth=False,
-                         priv=False,
+                         auth=auth,
+                         priv=priv,
                          authorative_engine=encoder.v3.AuthorativeEngine(
-                             id=b'engine_id',
+                             id=engine_id,
                              boots=0,
                              time=0),
-                         user='user',
-                         context=common.Context(
-                             engine_id=b'engine_id',
-                             name='name'),
+                         user=username,
+                         context=context,
                          pdu=encoder.v2c.BasicPdu(
                              request_id=12345,
                              error=common.Error(
@@ -269,13 +325,14 @@ async def test_listener_receive_trap_v3(udp_addr, data):
                                        value=(1, 2, 3, 4)),
                                    *data]))
 
-    msg_bytes = encoder.encode(msg)
+    msg_bytes = encoder.encode(msg,
+                               auth_key=auth_key,
+                               priv_key=priv_key)
     sender.send(msg_bytes)
 
     user, context, tr = await trap_queue.get()
-    assert user == 'user'
-    assert context.engine_id == b'engine_id'
-    assert context.name == 'name'
+    assert user == username
+    assert context == context
     assert tr.cause is None
     assert tr.oid == (1, 2, 3, 4)
     assert tr.timestamp == 12345
@@ -324,30 +381,63 @@ async def test_sender_send_inform_v2c(udp_addr, data):
 
 
 @pytest.mark.parametrize("data", [list(_data('v3'))])
-async def test_sender_send_inform_v3(udp_addr, data):
+@pytest.mark.parametrize('auth, priv', [
+    (False, False),
+    (True, False),
+    (True, True)])
+@pytest.mark.parametrize('auth_type', common.AuthType)
+async def test_sender_send_inform_v3(udp_addr, data, auth, auth_type, priv):
+    username = 'user_xyz'
+    context = common.Context(engine_id=b'ctx_engine_id',
+                             name='name')
+    auth_pass = 'authpass'
+    priv_pass = 'privpass'
+    auth_key = (key.create_key(key_type=key.KeyType[auth_type.name],
+                               password=auth_pass,
+                               engine_id=context.engine_id)
+                if auth else None)
+    priv_key = (key.create_key(key_type=key.KeyType.DES,
+                               password=priv_pass,
+                               engine_id=context.engine_id)
+                if priv else None)
+
+    def on_auth_key(eid, usr):
+        return auth_key
+
+    def on_priv_key(eid, usr):
+        return priv_key
+
     listener = await udp.create(local_addr=udp_addr)
 
     sender = await trap.create_v3_trap_sender(
-        udp_addr, common.Context(engine_id=b'engine_id', name='name'))
+        remote_addr=udp_addr,
+        context=context,
+        user=common.User(
+            name=username,
+            auth_type=auth_type if auth else None,
+            auth_password=auth_pass if auth else None,
+            priv_type=common.PrivType.DES if priv else None,
+            priv_password=priv_pass if priv else None))
 
     inform_req = common.Inform(data=data)
     res_f = sender.async_group.spawn(sender.send_inform, inform_req)
 
     msg_bytes, addr = await listener.receive()
-    msg = encoder.decode(msg_bytes)
+    msg = encoder.decode(msg_bytes,
+                         auth_key_cb=on_auth_key,
+                         priv_key_cb=on_priv_key)
     req_id = msg.pdu.request_id
 
     assert msg.type == encoder.v3.MsgType.INFORM_REQUEST
     assert msg.id is not None
     assert msg.reportable is False
-    assert msg.auth is False
-    assert msg.priv is False
-    assert msg.authorative_engine.id == b'engine_id'
+    assert msg.auth is auth
+    assert msg.priv is priv
+    assert msg.authorative_engine.id == context.engine_id
     assert msg.authorative_engine.boots == 0
     assert msg.authorative_engine.time is not None
-    assert msg.user == 'public'
-    assert msg.context.engine_id == b'engine_id'
-    assert msg.context.name == 'name'
+    assert msg.user == username
+    assert msg.context == context
     assert msg.pdu.error.type == common.ErrorType.NO_ERROR
     assert msg.pdu.data == data
     assert not res_f.done()
@@ -356,23 +446,23 @@ async def test_sender_send_inform_v3(udp_addr, data):
         type=encoder.v3.MsgType.RESPONSE,
         id=msg.id,
         reportable=False,
-        auth=False,
-        priv=False,
+        auth=auth,
+        priv=priv,
         authorative_engine=encoder.v3.AuthorativeEngine(
-            id=b'engine_id',
+            id=context.engine_id,
             boots=0,
             time=0),
-        user='public',
-        context=common.Context(
-            engine_id=b'engine_id',
-            name='name'),
+        user=username,
+        context=context,
         pdu=encoder.v3.BasicPdu(
             request_id=req_id,
             error=common.Error(
                 type=common.ErrorType.NO_ERROR,
                 index=0),
             data=data))
-    msg_bytes = encoder.encode(msg)
+    msg_bytes = encoder.encode(msg,
+                               auth_key=auth_key,
+                               priv_key=priv_key)
     listener.send(msg_bytes, addr)
 
     send_inform_res = await res_f
@@ -427,7 +517,40 @@ async def test_listener_receive_inform_v2c(udp_addr, data):
 
 
 @pytest.mark.parametrize("data", [list(_data('v3'))])
-async def test_listener_receive_inform_v3(udp_addr, data):
+@pytest.mark.parametrize('auth, priv', [(False, False),
+                                        (True, False),
+                                        (True, True)])
+@pytest.mark.parametrize('auth_type', common.AuthType)
+async def test_listener_receive_inform_v3(udp_addr, data, auth, auth_type,
+                                          priv):
+    username = 'user_xyz'
+    engine_id = b'engine_id'
+    context = common.Context(engine_id=b'ctx_engine_id',
+                             name='name')
+    auth_pass = 'authpass'
+    priv_pass = 'privpass'
+    auth_key = (key.create_key(key_type=key.KeyType[auth_type.name],
+                               password=auth_pass,
+                               engine_id=engine_id)
+                if auth else None)
+    priv_key = (key.create_key(key_type=key.KeyType.DES,
+                               password=priv_pass,
+                               engine_id=engine_id)
+                if priv else None)
+
+    def on_auth_key(eid, usr):
+        return auth_key
+
+    def on_priv_key(eid, usr):
+        return priv_key
+
+    user = common.User(
+        name=username,
+        auth_type=auth_type if auth else None,
+        auth_password=auth_pass if auth else None,
+        priv_type=common.PrivType.DES if priv else None,
+        priv_password=priv_pass if priv else None)
+
     data = [common.TimeTicksData(name=(1, 3, 6, 1, 2, 1, 1, 3, 0),
                                  value=12345),
             common.ObjectIdData(name=(1, 6, 3, 1, 1, 4, 1, 0),
@@ -436,32 +559,28 @@ async def test_listener_receive_inform_v3(udp_addr, data):
 
     def inform_cb(addr, user, context, inform_req):
         assert addr == sender.info.local_addr
-        assert user == 'user'
-        assert context.engine_id == b'engine_id'
-        assert context.name == 'name'
+        assert user == username
+        assert context == context
         assert inform_req.data == data
 
-    listener = await trap.create_trap_listener(local_addr=udp_addr,
-                                               v3_inform_cb=inform_cb,
-                                               users=[common.User('user', None,
-                                                                  None, None,
-                                                                  None)])
+    listener = await trap.create_trap_listener(
+        local_addr=udp_addr,
+        v3_inform_cb=inform_cb,
+        users=[user])
 
     sender = await udp.create(remote_addr=udp_addr)
 
     msg = encoder.v3.Msg(type=encoder.v3.MsgType.INFORM_REQUEST,
                          id=4531,
                          reportable=False,
-                         auth=False,
-                         priv=False,
+                         auth=auth,
+                         priv=priv,
                          authorative_engine=encoder.v3.AuthorativeEngine(
-                             id=b'engine_id',
+                             id=engine_id,
                              boots=0,
                              time=0),
-                         user='user',
-                         context=common.Context(
-                             engine_id=b'engine_id',
-                             name='name'),
+                         user=username,
+                         context=context,
                          pdu=encoder.v2c.BasicPdu(
                              request_id=4531,
                              error=common.Error(
@@ -469,16 +588,19 @@ async def test_listener_receive_inform_v3(udp_addr, data):
                                  index=0),
                              data=data))
 
-    msg_bytes = encoder.encode(msg)
+    msg_bytes = encoder.encode(msg,
+                               auth_key=auth_key,
+                               priv_key=priv_key)
     sender.send(msg_bytes)
 
     msg_bytes, _ = await sender.receive()
-    msg = encoder.decode(msg_bytes)
+    msg = encoder.decode(msg_bytes,
+                         auth_key_cb=on_auth_key,
+                         priv_key_cb=on_priv_key)
 
     assert isinstance(msg, encoder.v3.Msg)
     assert msg.type == encoder.v3.MsgType.RESPONSE
-    assert msg.context.engine_id == b'engine_id'
-    assert msg.context.name == 'name'
+    assert msg.context == context
     assert isinstance(msg.pdu, encoder.v3.BasicPdu)
     assert msg.pdu.request_id == 4531
     assert msg.pdu.error.type == common.ErrorType.NO_ERROR
