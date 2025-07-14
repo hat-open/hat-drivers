3ec19db3ee33bf6058243d339aed710f00b5a9ff
bozo.kopic <bozo@kopic.xyz>
2025-07-15 00:50:27 +0200

    iec61850 manager device_readout

--- src_py/hat/drivers/iec61850/manager/device_readout/readout.py
+++ src_py/hat/drivers/iec61850/manager/device_readout/readout.py
@@ -3,14 +3,12 @@ import logging
 
 from hat import aio
 from hat import asn1
-from hat import json
 
 from hat.drivers import mms
 from hat.drivers import tcp
 from hat.drivers.iec61850 import encoder
 from hat.drivers.iec61850.manager.device_readout import common
-from hat.drivers.iec61850.manager.device_readout.parser import (get_cdc_data,
-                                                                get_cdc_commands)  # NOQA
+from hat.drivers.iec61850.manager.device_readout.parser import get_device_conf
 from hat.drivers.iec61850.manager.device_readout.client import Client
 
 
@@ -137,172 +135,13 @@ async def readout(addr: tcp.Address,
     finally:
         await aio.uncancellable(conn.async_close())
 
-    cdc_data = get_cdc_data(value_types, dataset_data_refs)
-    cdc_commands = get_cdc_commands(value_types, cmd_models)
-
-    return {
-        'connection': _get_connection_conf(addr=addr,
-                                           tsel=remote_tsel,
-                                           ssel=remote_ssel,
-                                           psel=remote_psel,
-                                           ap_title=remote_ap_title,
-                                           ae_qualifier=remote_ae_qualifier),
-        'value_types': _get_value_types_conf(value_types),
-        'datasets': _get_datasets_conf(dataset_data_refs),
-        'rcbs': _get_rcbs_conf(rcb_attr_values),
-        'data': _get_data_conf(cdc_data),
-        'commands': _get_commands_conf(cdc_commands)}
-
-
-def _get_connection_conf(addr: tcp.Address,
-                         tsel: int | None,
-                         ssel: int | None,
-                         psel: int | None,
-                         ap_title: asn1.ObjectIdentifier | None,
-                         ae_qualifier: int | None
-                         ) -> json.Data:
-    connection_conf = {'host': addr.host,
-                       'port': addr.port}
-
-    if tsel is not None:
-        connection_conf['tsel'] = tsel
-
-    if ssel is not None:
-        connection_conf['ssel'] = ssel
-
-    if psel is not None:
-        connection_conf['psel'] = psel
-
-    if ap_title is not None:
-        connection_conf['ap_title'] = ap_title
-
-    if ae_qualifier is not None:
-        connection_conf['ae_qualifier'] = ae_qualifier
-
-    return connection_conf
-
-
-def _get_value_types_conf(value_types: dict[common.RootDataRef,
-                                            common.ValueType | None]
-                          ) -> json.Data:
-    return [{'logical_device': root_data_ref.logical_device,
-             'logical_node': root_data_ref.logical_node,
-             'fc': root_data_ref.fc,
-             'name': root_data_ref.name,
-             'type': (_value_type_to_json(value_type) if value_type is not None
-                      else None)}
-            for root_data_ref, value_type in value_types.items()]
-
-
-def _get_datasets_conf(dataset_data_refs: dict[common.DatasetRef,
-                                               Collection[common.DataRef]]
-                       ) -> json.Data:
-    return [{'ref': _dataset_ref_to_json(dataset_ref),
-             'values': [_data_ref_to_json(data_ref)
-                        for data_ref in data_refs]}
-            for dataset_ref, data_refs in dataset_data_refs.items()]
-
-
-def _get_rcbs_conf(rcb_attr_values: dict[common.RcbRef,
-                                         dict[common.RcbAttrType,
-                                              common.RcbAttrValue]]
-                   ) -> json.Data:
-    return [
-        {'ref': _rcb_ref_to_json(rcb_ref),
-         'report_id': attr_values[common.RcbAttrType.REPORT_ID],
-         'dataset': (
-            _dataset_ref_to_json(attr_values[common.RcbAttrType.DATASET])
-            if attr_values[common.RcbAttrType.DATASET] is not None
-            else None),
-         'conf_revision': attr_values[common.RcbAttrType.CONF_REVISION],
-         'optional_fields': [
-            i.name
-            for i in attr_values[common.RcbAttrType.OPTIONAL_FIELDS]],
-         'buffer_time': attr_values[common.RcbAttrType.BUFFER_TIME],
-         'trigger_options': [
-            i.name
-            for i in attr_values[common.RcbAttrType.TRIGGER_OPTIONS]],
-         'integrity_period': attr_values[common.RcbAttrType.INTEGRITY_PERIOD],
-         'uneditable': []}
-        for rcb_ref, attr_values in rcb_attr_values.items()]
-
-
-def _get_data_conf(cdc_data: dict[common.CdcDataRef, common.CdcData]
-                   ) -> json.Data:
-    return [{'ref': _cdc_data_ref_to_json(i_ref),
-             'cdc': (i.cdc.name if i.cdc else None),
-             'values': [{'name': value.name,
-                         'fc': value.fc,
-                         'datasets': [_dataset_ref_to_json(ds)
-                                      for ds in value.datasets],
-                         'writable': value.writable}
-                        for value in i.values]}
-            for i_ref, i in cdc_data.items()]
-
-
-def _get_commands_conf(cdc_commands: dict[common.CommandRef, common.CdcCommand]
-                       ) -> json.Data:
-    return [{'ref': _command_ref_to_json(cmd_ref),
-             'model': cdc_cmd.model.name,
-             'with_operate_time': cdc_cmd.with_operate_time,
-             'cdc': cdc_cmd.cdc.name}
-            for cmd_ref, cdc_cmd in cdc_commands.items()]
-
-
-def _value_type_to_json(value_type: common.ValueType
-                        ) -> json.Data:
-    if isinstance(value_type, common.BasicValueType):
-        return value_type.name
-
-    if isinstance(value_type, common.AcsiValueType):
-        return value_type.name
-
-    if isinstance(value_type, common.ArrayValueType):
-        return {'type': 'ARRAY',
-                'element_type': _value_type_to_json(value_type.type),
-                'length': value_type.length}
-
-    if isinstance(value_type, common.StructValueType):
-        return {'type': 'STRUCT',
-                'elements': [{'name': i_name,
-                              'type': _value_type_to_json(i_type)}
-                             for i_name, i_type in value_type.elements]}
-
-    raise TypeError('unsupported value type')
-
-
-def _data_ref_to_json(ref: common.DataRef
-                      ) -> json.Data:
-    return {'logical_device': ref.logical_device,
-            'logical_node': ref.logical_node,
-            'fc': ref.fc,
-            'names': list(ref.names)}
-
-
-def _dataset_ref_to_json(ref: common.PersistedDatasetRef
-                         ) -> json.Data:
-    return {'logical_device': ref.logical_device,
-            'logical_node': ref.logical_node,
-            'name': ref.name}
-
-
-def _rcb_ref_to_json(ref: common.RcbRef
-                     ) -> json.Data:
-    return {'logical_device': ref.logical_device,
-            'logical_node': ref.logical_node,
-            'type': ref.type.name,
-            'name': ref.name}
-
-
-def _command_ref_to_json(ref: common.CommandRef
-                         ) -> json.Data:
-    return {'logical_device': ref.logical_device,
-            'logical_node': ref.logical_node,
-            'name': ref.name}
-
-
-def _cdc_data_ref_to_json(ref: common.CdcDataRef
-                          ) -> json.Data:
-    return {'logical_device': ref.logical_device,
-            'logical_node': ref.logical_node,
-            'names': list(ref.names)}
+    return get_device_conf(addr=addr,
+                           tsel=remote_tsel,
+                           ssel=remote_ssel,
+                           psel=remote_psel,
+                           ap_title=remote_ap_title,
+                           ae_qualifier=remote_ae_qualifier,
+                           value_types=value_types,
+                           dataset_data_refs=dataset_data_refs,
+                           rcb_attr_values=rcb_attr_values,
+                           cmd_models=cmd_models)
