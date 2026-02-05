9ce063286d8369b074e7ea1eaf784a68713201c2
bozo.kopic <bozo@kopic.xyz>
2026-02-05 18:28:20 +0100

    test iec61850 manager file_readout fix

--- test_pytest/test_iec61850/test_manager/test_file_readout/test_file_readout.py
+++ test_pytest/test_iec61850/test_manager/test_file_readout/test_file_readout.py
@@ -128,9 +128,9 @@ def test_1(validator):
 
     # rcb with rcbID="" get generated rptID as ld/ln.BR/RP.name
     rcbs = device_json['rcbs']
-    assert rcbs[0]['report_id'] == 'DemoMeasurement/LLN0.BR.brcb101'
+    assert rcbs[0]['report_id'] == 'DemoMeasurement/LLN0$BR$brcb101'
     assert rcbs[0]['ref']['name'] == 'brcb101'
-    assert rcbs[1]['report_id'] == 'DemoMeasurement/LLN0.RP.urcb101'
+    assert rcbs[1]['report_id'] == 'DemoMeasurement/LLN0$RP$urcb101'
     assert rcbs[1]['ref']['name'] == 'urcb101'
 
     # no dynamic rcb editable attributes
